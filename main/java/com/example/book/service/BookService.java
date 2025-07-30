cbcc21cae40ac8f3d5f0b326d0c9e995f9404e75
nospring2902 <163527486+nospring2902@users.noreply.github.com>
2025-07-30 14:52:00 +0700

    demo JPA

---
+++ main/java/com/example/book/service/BookService.java
@@ -0,0 +1,45 @@
+package com.example.book.service;
+
+import com.example.book.exception.ResourceNotFoundException;
+import com.example.book.model.Book;
+import com.example.book.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Book not found with id: " + id));
+    }
+
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Book updateBook(Long id, Book bookDetails) {
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Book not found with id: " + id));
+        
+        book.setTitle(bookDetails.getTitle());
+        book.setAuthor(bookDetails.getAuthor());
+        book.setPrice(bookDetails.getPrice());
+        
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Book not found with id: " + id));
+        bookRepository.delete(book);
+    }
+}
